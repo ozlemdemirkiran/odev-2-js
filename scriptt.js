1d1626e625938be9b75551926b4b017dc9ec966a
ozlem <ozlem.demirkiran3460@gmail.com>
2023-09-11 23:00:25 +0300

    fff

---
+++ scriptt.js
@@ -0,0 +1,82 @@
+
+// popup hidden / block
+let urunEkle = document.getElementById("urun-ekle");
+let artiButonu = document.querySelector(".arti-butonu");
+let popupKapsayici = document.querySelector(".popup-kapsayici");
+let carpiButonu = document.querySelector(".carpi-butonu");
+
+function drawerMenu(){
+    popupKapsayici.classList.replace("hidden", "block");
+}
+
+function popupGizleme(){
+    popupKapsayici.classList.replace("block", "hidden");
+    urunAdi.value = "";
+    urunFiyati.value = "";
+    urunAciklamasi.value = "";
+}
+/* carpiButonu.addEventListener("click", () => {
+    popupKapsayici.classList.replace("block", "hidden")
+    urunAdi.value = "";
+    urunFiyati.value = "";
+    urunAciklamasi.value = "";
+});
+*/
+
+artiButonu.addEventListener("click", drawerMenu);
+urunEkle.addEventListener("click", drawerMenu);
+carpiButonu.addEventListener("click", popupGizleme);
+
+
+// product
+
+const urunOlusturmaButonu = document.querySelector("#urun-olustur-butonu");
+const eklenmisUrun = document.getElementById("added-product");
+
+const urunAdi = document.getElementById("urun-adi");
+const urunFiyati = document.getElementById("urun-fiyati");
+const urunAciklamasi = document.getElementById("urun-aciklamasi");
+
+
+urunOlusturmaButonu.addEventListener("click", () => {
+
+    let newProduct = {
+        productName: urunAdi.value,
+        price: urunFiyati.value,
+        description: urunAciklamasi.value,
+    }
+
+    let card = `
+    <div class="w-1/4 p-4 ml-16">
+        <div class="shadow-md flex flex-col justify-center items-center border-2 rounded-lg h-auto">
+            <h2>${newProduct.productName}</h2>
+            <div class="flex gap-1 align-center">
+                <p>${newProduct.price}</p>
+                <img src="./img/turkish-lira.png" class="h-3">
+            </div>
+            <p>${newProduct.description}</p>
+            <a class="pt-3 text-teal-600" href="#">ÜRÜNÜ DÜZENLE</a>
+        </div>
+    </div>
+    `
+    
+    eklenmisUrun.innerHTML += card;
+
+    urunAdi.value = "";
+    urunFiyati.value = "";
+    urunAciklamasi.value = "";
+});
+
+
+
+
+    //length
+    
+// let containerr = document.querySelector(".container");
+
+// let toplamLength = `<h1 class="font-bold mx-10 mt-3">Ürün Listesi (${eklenmisUrun.children.length})</h1>`
+
+// containerr.innerHTML = toplamLength;
+// // containerr.innerText += toplamLength;
+// containerr.innerText += eklenmisUrun.children.length;
+
